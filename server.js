294b6fa7d1c79556904f82b98a867450094c86e1
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-10-01 21:02:38 -0300

    Criação do servidor web

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const servidor = http.createServer(function (req, resp) {
+    resp.end(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Casa do Código</h1>
+            </body>
+        </html>
+    `);
+});
+servidor.listen(3000);
