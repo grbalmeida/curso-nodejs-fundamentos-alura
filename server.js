cc2b90e02640e6cdbabe6a360e9dbe4baf849349
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-10-01 22:42:50 -0300

    Express em ação

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function (req, resp) {
-    let html = '';
-
-    if (req.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <header>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="http://localhost:3000">Home</a>
-                                </li>
-                                <li>
-                                    <a href="http://localhost:3000/livros">Livros</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </header>
-                    <h1>Casa do Código</h1>
-                </body>
-            </html>
-        `;
-    } else if (req.url == '/livros') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <header>
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="http://localhost:3000">Home</a>
-                                </li>
-                                <li>
-                                    <a href="http://localhost:3000/livros">Livros</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </header>
-                    <h1>Listagem de livros</h1>
-                </body>
-            </html>
-        `;
-    }
-
-    resp.end(html);
+app.get('/', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="http://localhost:3000">Home</a>
+                            </li>
+                            <li>
+                                <a href="http://localhost:3000/livros">Livros</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <h1>Casa do Código</h1>
+            </body>
+        </html>
+    `);
 });
-servidor.listen(3000);
+
+app.get('/livros', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="http://localhost:3000">Home</a>
+                            </li>
+                            <li>
+                                <a href="http://localhost:3000/livros">Livros</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <h1>Listagem de livros</h1>
+            </body>
+        </html>
+    `);
+});
+
+app.listen(3000, function() {
+    console.log('Servidor rodando na porta 3000');
+});
