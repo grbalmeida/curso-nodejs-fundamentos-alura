65b1ab39ef5c1689f3b00b7948fcddd4c3cc9287
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-10-01 22:22:58 -0300

    Organização do projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+### Comando para instalar Express
+
+```
+npm install express@4.16.3 --save-exact
+```
--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Casa do Código</h1>
-            </body>
-        </html>
-    `);
+    let html = '';
+
+    if (req.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="http://localhost:3000">Home</a>
+                                </li>
+                                <li>
+                                    <a href="http://localhost:3000/livros">Livros</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <h1>Casa do Código</h1>
+                </body>
+            </html>
+        `;
+    } else if (req.url == '/livros') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="http://localhost:3000">Home</a>
+                                </li>
+                                <li>
+                                    <a href="http://localhost:3000/livros">Livros</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <h1>Listagem de livros</h1>
+                </body>
+            </html>
+        `;
+    }
+
+    resp.end(html);
 });
 servidor.listen(3000);
